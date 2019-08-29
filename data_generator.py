6d7cee427a2ed0bc1c79967030b7d3cb3f05ba31
sumitdaroch <sumitkumardaroch@gmail.com>
2019-08-30 00:18:06 +0530

    Model is ready for training

---
+++ data_generator.py
@@ -0,0 +1,81 @@
+import os 
+import glob
+import numpy as np
+from keras import Input, layers
+from keras.layers.merge import add
+from keras.models import Model
+from keras.layers import LSTM, Embedding, TimeDistributed, Dense, RepeatVector,Activation, Flatten, Reshape, concatenate, Dropout, BatchNormalization
+from data_preprocessing_2 import wordtoix,ixtoword,vocab_size,max_length
+
+def data_generator(descriptions, photos, wordtoix, max_length, num_photos_per_batch):
+    X1, X2, y = list(), list(), list()
+    n=0
+    # loop for ever over images
+    while 1:
+        for key, desc_list in descriptions.items():
+            n+=1
+            # retrieve the photo feature
+            photo = photos[key+'.jpg']
+            for desc in desc_list:
+                # encode the sequence
+                seq = [wordtoix[word] for word in desc.split(' ') if word in wordtoix]
+                # split one sequence into multiple X, y pairs
+                for i in range(1, len(seq)):
+                    # split into input and output pair
+                    in_seq, out_seq = seq[:i], seq[i]
+                    # pad input sequence
+                    in_seq = pad_sequences([in_seq], maxlen=max_length)[0]
+                    # encode output sequence
+                    out_seq = to_categorical([out_seq], num_classes=vocab_size)[0]
+                    # store
+                    X1.append(photo)
+                    X2.append(in_seq)
+                    y.append(out_seq)
+            # yield the batch data
+            if n==num_photos_per_batch:
+                yield [[array(X1), array(X2)], array(y)]
+                X1, X2, y = list(), list(), list()
+                n=0
+  #-------------------------------------------------------------------------------------------------------------
+  
+
+# Load Glove vectors
+glove_dir = '../glove'
+embeddings_index = {} # empty dictionary
+f = open(os.path.join(glove_dir, 'glove.6B.200d.txt'), encoding="utf-8")
+
+for line in f:
+    values = line.split()
+    word = values[0]
+    coefs = np.asarray(values[1:], dtype='float32')
+    embeddings_index[word] = coefs
+f.close()
+print('Found %s word vectors.' % len(embeddings_index))    
+
+#------------------------------------------------------------------------------------------------------------------
+embedding_dim = 200
+
+# Get 200-dim dense vector for each of the 10000 words in out vocabulary
+embedding_matrix = np.zeros((vocab_size, embedding_dim))
+
+for word, i in wordtoix.items():
+    #if i < max_words:
+    embedding_vector = embeddings_index.get(word)
+    if embedding_vector is not None:
+        # Words not found in the embedding index will be all zeros
+        embedding_matrix[i] = embedding_vector    
+#------------------------------------------------------------------------------------------------------------------
+
+inputs1 = Input(shape=(2048,))
+fe1 = Dropout(0.5)(inputs1)
+fe2 = Dense(256, activation='relu')(fe1)
+inputs2 = Input(shape=(max_length,))
+se1 = Embedding(vocab_size, embedding_dim, mask_zero=True)(inputs2)
+se2 = Dropout(0.5)(se1)
+se3 = LSTM(256)(se2)
+decoder1 = add([fe2, se3])
+decoder2 = Dense(256, activation='relu')(decoder1)
+outputs = Dense(vocab_size, activation='softmax')(decoder2)
+model = Model(inputs=[inputs1, inputs2], outputs=outputs)              
+
+print(model.summary())
